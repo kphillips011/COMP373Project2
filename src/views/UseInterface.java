ebd754c332fd38e266ce7b4853b4973f8cfa817f
kendall <kendallnicole011@gmail.com>
2021-02-22 23:37:26 -0600

    import ArrayList

--- src/views/UseInterface.java
+++ src/views/UseInterface.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+
 public interface UseInterface {
     public boolean isInUseDuringinterval();
-    public assignFacilityToUse();
+    public boolean assignFacilityToUse();
     public Facility vacateFacility();
-    public ArrayList<Inspection> listInspections(); 
+    public ArrayList<Inspection> listInspections();
     public ArrayList<Use> listActualUsage();
     public float calcUsageRate();
 }
