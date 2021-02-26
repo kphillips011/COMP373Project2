ce3a95e5ce79aeb60bd5d13a3504f093a2685bf9
kendall <kendallnicole011@gmail.com>
2021-02-26 12:07:53 -0600

    structure changes for use/use interface

--- src/models/Use.java
+++ src/models/Use.java
@@ -4,22 +4,27 @@ import java.util.Date;
 
 public class Use implements UseInterface {
     String details;
-    boolean inUse;
-    Facility facility;
+    boolean inUse = false;
+    Facility facility; Date date; LocalTime duration; Room room;
 
     public Use(String details, Date date, LocalTime duration, Room room, Facility facility) {
-        super(date, duration, room, facility);
+        //super(date, duration, room, facility);
         inUse = true;
         this.details = details;
+        this.facility = facility; this.date = date; this.duration = duration; this.room = room;
     }
-    public boolean isInUseDuringinterval() { return inUse; } //returns if facility is in use
 
+    @Override
+    public boolean isInUseDuringInterval() { return inUse; } //returns if facility is in use
+
+    @Override
     public boolean assignFacilityToUse(Facility facility)
     {
     inUse = true;
     return inUse;
     }
 
+    @Override
     public float calcUsageRate() {
         // TODO
         return 0;
--- src/views/UseInterface.java
+++ src/views/UseInterface.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public interface UseInterface {
-    public boolean isInUseDuringinterval(); // to check if a room/facility is in use
+    public boolean isInUseDuringInterval(); // to check if a room/facility is in use
     public boolean assignFacilityToUse(Facility facility); // Assigning a facility to whether or not it is in use
     public float calcUsageRate(); // used to find how often/how much a facility/room/building is used.
 }
