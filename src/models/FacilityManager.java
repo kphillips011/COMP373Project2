81c22079aae24911ecbc672c0ab99213806c7e63
kendall <kendallnicole011@gmail.com>
2021-02-26 12:51:49 -0600

    updated FacilityManager

--- src/models/FacilityManager.java
+++ src/models/FacilityManager.java
@@ -19,7 +19,7 @@ public class FacilityManager {
     public ArrayList<Use> listActualUsage() { return new ArrayList<Use>(); } // Lists all uses of rooms/facilities/buildings etc.
 
     // TODO -- is this the right place
-    public ArrayList<Inspection> listInspections() { return new ArrayList<Inspection>(); } // Lists all information about inspection
+    public ArrayList<Maintenance.Inspection> listInspections() { return new ArrayList<Maintenance.Inspection>(); } // Lists all information about inspection
 
     // TODO -- is this the right place
     // I think this should list all Maintenance objects where completed == False
