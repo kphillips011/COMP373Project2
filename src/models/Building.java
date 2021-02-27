cfdbe94990a9307b34a6e6469ffcd7f865f5b1a5
Trey Roche <proche@luc.edu>
2021-02-27 11:01:31 -0600

    Added buolding constructor and unit test for Room

--- src/models/Building.java
+++ src/models/Building.java
@@ -5,4 +5,11 @@ public class Building {
     String buildingName;
     String address;
     ArrayList<Room> rooms;
+
+    public Building(int buildingID, String buildingName, String address, ArrayList<Room> rooms){
+        this.buildingID = buildingID;
+        this.buildingName = buildingName;
+        this.address = address;
+        this.rooms = rooms;
+    }
 }
