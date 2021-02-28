9990d481701a0784dc90edc2e3b3ebb05b1db729
kendall <kendallnicole011@gmail.com>
2021-02-28 17:21:27 -0600

    trying to fix package problems

--- src/models/Room.java
+++ src/models/Room.java
@@ -1,4 +1,4 @@
-//package models;
+package models;
 
 public class Room {
     String roomID; // will be composed of building code and room number. Example: Cuneo 313 -> 001313 if Cuneo has building code 001
--- src/views/FacilityInterface.java
+++ src/views/FacilityInterface.java
@@ -1,4 +1,4 @@
-//package views;
+package views;
 
 import java.util.ArrayList;
 import java.util.Date;
