a46de4a39f4f7be2702ae42fde02996c556602e5
Trey Roche <proche@luc.edu>
2021-02-28 21:48:35 -0600

    set up switch statement in client

--- src/views/FacilitySystemClient.java
+++ src/views/FacilitySystemClient.java
@@ -1,7 +1,7 @@
 package views;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.*;
 
 import dataAccess.DataReader;
 import models.*;
@@ -11,11 +11,30 @@ public class FacilitySystemClient {
         // TODO
         // connect Facility, Use, Maintenance classes to client
         DataReader dr = new DataReader();
+        HashMap<String,Object> input;
         try{ 
-            HashMap<String,Object> input = dr.read();
+            input = dr.read();
         }
         catch (IOException e){
             throw new IOException();
+        }
+        Iterator i = input.entrySet().iterator();
+        while(i.hasNext()){
+            Map.Entry element = (Map.Entry)i.next();
+            String key = element.getKey().toString();
+            Object val = element.getValue();
+            switch(key){
+                case "Facility":
+                    break;
+                case "Building":
+                    break;
+                case "Room":
+                    break;
+                case "MaintenanceRequest":
+                    break;
+                case "Use":
+                    break;
+            }
         }
     }
 }
