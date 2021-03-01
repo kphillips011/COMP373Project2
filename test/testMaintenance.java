e4a22a9dfa74af8dc5963595835b5f1e78f54ce7
kendall <kendallnicole011@gmail.com>
2021-02-28 19:30:57 -0600

    updating maintenance test methods

--- test/testMaintenance.java
+++ test/testMaintenance.java
@@ -1,34 +1,50 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.beans.IndexedPropertyChangeEvent;
+import java.util.Date;
+
+import models.Building;
+import models.Facility;
 import models.Maintenance;
+import models.Room;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class testMaintenance {
     private Maintenance m;
+    private Maintenance.Inspection i;
 
     @BeforeEach
     public void setUp() {
 
-        m = new Maintenance();
+        m = new Maintenance(new Facility(1, "facility 1",
+                "address 1"), "Leaky faucet", 100.00, new Date(),
+                20.0, new Room(new Building(2, "building 2",
+                "address 2"), 2, 202));
+        i = m.new Inspection();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        m = null;
+        i = null;
     }
 
     @Test
     @DisplayName("Testing getMaintenanceCost")
     public void testGetMaintenanceCost() {
-        // TODO
+        assertEquals(100.00, m.getMaintenanceCost());
     }
 
     @Test
     @DisplayName("Testing testIsCompleted")
     public void testIsCompleted() {
         // TODO
-    }
-
-    @Test
-    @DisplayName("Testing listMaintenance")
-    public void testListMaintenance() {
-        // TODO
+        m.setCompleted();
+        assertEquals(true, m.isCompleted());
     }
 
     /* Inspection test */
