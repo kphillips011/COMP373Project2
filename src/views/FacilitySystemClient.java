3cce5653d7babc456138cce7dc644d905f98480c
Trey Roche <proche@luc.edu>
2021-02-28 22:30:03 -0600

    Remove last TODO

--- src/views/FacilitySystemClient.java
+++ src/views/FacilitySystemClient.java
@@ -8,8 +8,6 @@ import models.*;
 
 public class FacilitySystemClient {
     public static void main(String[] args) throws IOException {
-        // TODO
-        // connect Facility, Use, Maintenance classes to client
         DataReader dr = new DataReader();
         HashMap<String,Object> input;
         try{ 
